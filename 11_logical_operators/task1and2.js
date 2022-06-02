1c4398c4feae0e35e1ebde29720e0f7e1406a0ea
Vikramaditya Bhatnagar <55340732+Vikram1997-hue@users.noreply.github.com>
2022-06-02 18:01:03 +0530

    Add files via upload

---
+++ 11_logical_operators/task1and2.js
@@ -0,0 +1,12 @@
+/*
+
+TASK 1:
+if("0") will be truthy, since "0" is not an empty string
+
+*/
+
+let result = prompt("What is the \"official\" name of JavaScript?", "Type here...");
+if(result == "ECMAScript")
+    alert("Right!");
+else
+    alert("You don\'t know? ECMAScript!");
---
+++ 11_logical_operators/task3.js
@@ -0,0 +1,7 @@
+let result = prompt("Please enter a number", "Type here...");
+if(+result > 0)
+    alert(1);
+else if(+result<0)
+    alert(-1);
+else
+    alert(0);
